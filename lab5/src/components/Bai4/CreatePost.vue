1fa3ae4c3576f7ed2990ab38ce25536bca666d7e
NTH-Nguyen <nthn300607@gmail.com>
2026-01-26 20:45:53 +0700

    upload

---
+++ lab5/src/components/Bai4/CreatePost.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <h2 class="">Tạo bài viết mới</h2>
+    <div class="container-fluid">
+        <input class="form-control mb-3" v-model="blog.title" placeholder="Tiêu đề bài viết">
+        <input class="form-control mb-3" v-model="blog.author" placeholder="Tên tác giả">
+        <textarea class="form-control mb-3" v-model="blog.content" placeholder="Nội dung bài viết"></textarea>
+        <button class="btn btn-primary align-items-end" @click="submitEv">
+            Đăng bài
+        </button>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['add-post']);
+
+const blog = ref({
+    title: "",
+    author: "",
+    content: ""
+});
+
+function submitEv() {
+    if (blog.value.title && blog.value.content && blog.value.author) {
+        emit('add-post', blog.value);
+
+        blog.value = {
+            title: "",
+            author: "",
+            content: ""
+        };
+    }
+}
+
+</script>
+
+<style></style>
