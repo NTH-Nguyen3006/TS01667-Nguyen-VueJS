96ec1d375cf8cca7e64766fbc3164182183f61ee
NTH-Nguyen <nthn300607@gmail.com>
2026-02-10 18:41:53 +0700

    update

--- lab8/src/components/AppFooter.vue
+++ lab8/src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="bg-white py-4 mt-auto border-top">
         <div class="container text-center text-muted">
-            <small>&copy; 2024 FPT Polytechnic - Lab 8. Developed with ❤️</small>
+            <small>&copy; 2026 FPT Polytechnic - Lab 8. Developed by Nguyen Bo Bo</small>
         </div>
     </footer>
 </template>
